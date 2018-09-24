87dfd817c7aec9334c75da6d8dc154f4e024d4b2
ken <block24block@gmail.com>
2018-09-24 16:53:44 +0800

    handy function to set margin-top

--- anchor.js
+++ anchor.js
@@ -3,14 +3,19 @@ Ateles('css', css => {
         klass_prefix = 'ateles-anchors-',
         id_prefix = 'ateles-anchor-',
         seq = 0,
-        next_seq = () => seq++
+        next_seq = () => seq++,
+        style = (cfg) => {
+            let s = cfg.top ? ['height: ', cfg.top, ';margin-top: -', cfg.top, ';'] : []
+            s.push(cfg.css)
+            return s.join('')
+        }
 
     return cfg => {
         let items = cfg.selector(),
             texts = {},
             klass = klass_prefix + next_seq()
 
-        css.text('.', klass, '{', cfg.css, '}')
+        css.text('.', klass, '{', style(cfg), '}')
 
         for (let i = 0; i < items.length; i++) {
             let item = items[i],
